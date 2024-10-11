c239fbc7b56c6394f456972705e24790a06d65ff
Shabareesha <arjunspy491@gmail.com>
2024-10-11 15:55:15 +0530

    Insert At The End

--- singlyLinkedList.c
+++ singlyLinkedList.c
@@ -15,12 +15,37 @@ void countNumOfNodes(struct node *head){
     while (ptr != NULL)
     {
         cnt++;
-        printf("%d", ptr->data);
         ptr = ptr->link;
-        printf("\n");
     }
+    printf("%d", cnt);
+};
 
-}
+void printNodesData(struct node *head){
+    if (head == NULL) printf("No nodes to count");
+    struct node *ptr = head;
+    int cnt = 0; //count
+
+    while (ptr != NULL)
+    {
+        printf("%d", ptr ->data);
+        printf("\n");
+        ptr = ptr->link;
+    }
+};
+
+void insertAtTheEnd(struct node *head, int data){
+
+    struct node *ptr, *temp;
+    ptr = head;
+    temp = malloc(sizeof(struct node));
+    temp -> data = data; // assigning data
+    temp -> link = NULL;
+
+    while(ptr->link != NULL){
+        ptr = ptr ->link;
+    }
+    ptr ->link = temp;
+};
 
 int main(){
     struct node *head = (struct node *)malloc(sizeof(struct node));
@@ -38,7 +63,9 @@ int main(){
     head ->link->link = crnt;
 
     // printf("%d", crnt ->data \n);
-    countNumOfNodes(head);
+    // countNumOfNodes(head);
+    insertAtTheEnd(head,60);
+    printNodesData(head);
     
     return 0;
 }
